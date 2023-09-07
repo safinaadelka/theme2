7fff9d0d95d05741064784846f7f579741fc1061
safinaadelka <lodgevvv@gmail.com>
2023-09-07 12:39:05 +0300

    sm2

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+let = city = prompt("Название города?")
+let = year = Number(prompt("В каком году был основан город?"))
+let = population = Number(prompt("Население города?"))
+
+let age
+age = 2023 - year
+alert("Городу " + city + " исполнилось " + age + " лет с момента образования. Население " + population + " человек" )
+
+
+
+console.log(city)
+console.log(year)
+console.log(population)
+console.log("Городу " + city + " исполнилось " + age + " лет с момента образования. Население " + population + " человек")
